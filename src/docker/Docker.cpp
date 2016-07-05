6c677074bfceb5940ab8c24259066b1289afd023
Przemysław Walkowiak <przemyslaw.walkowiak@put.poznan.pl>
2016-07-05 19:05:30 +0200

    loads Sphinx::Docker::v2 into Sphinx::Docker namespace

---
+++ src/docker/Docker.cpp
@@ -0,0 +1,4 @@
+
+#include "Docker.h"
+
+
---
+++ src/docker/Docker.h
@@ -0,0 +1,11 @@
+
+#pragma once
+#include "v2/DockerClient.h"
+
+namespace Sphinx {
+namespace Docker {
+
+using namespace v2;
+
+} // namespace Docker
+} // namespace Sphinx
