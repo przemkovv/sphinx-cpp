81fab57cc8949b2113184b71ae7696d1b5b5c066
Przemysław Walkowiak <przemyslaw.walkowiak@put.poznan.pl>
2016-07-18 17:38:45 +0200

    removes try-catch clause in main function

--- src/app/sphinx.cpp
+++ src/app/sphinx.cpp
@@ -8,14 +8,6 @@
 
 int main(int argc, char *argv[])
 {
-  try {
     Sphinx::Application sphinx{{argv + 1, argv + argc}};
-    // sphinx.init(argc, argv);
     return sphinx.run();
-  }
-  catch (std::exception &e) {
-    std::cout << e.what() << std::endl;
-    throw e;
-    //return -1;
-  }
 }
